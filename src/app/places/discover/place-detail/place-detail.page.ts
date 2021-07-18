1ef18af6acaae5cb66d022eebdad8e5f9c00ee69
Yi An Chen <ianchen0419@gmail.com>
2021-07-18 20:40:33 +0800

    back button of bunch page

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-detail',
@@ -9,12 +11,23 @@ import { NavController } from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
+  place: Place;
+
   constructor(
-    private router: Router,
     private navCtrl: NavController,
+    private route: ActivatedRoute,
+    private placesService: PlacesService,
   ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if(!paramMap.has('placeId')){
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+
+      this.place=this.placesService.getPlace(paramMap.get('placeId'));
+    });
   }
 
   onBookPlace(){
