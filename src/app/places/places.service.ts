acdd0b1ce4257ec44a30727127a17b0bec36d377
Yi An Chen <ianchen0419@gmail.com>
2021-07-18 14:13:11 +0800

    place service and model

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Place } from './place.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Manhattan Mansion',
+      'In the heart of New Your City.',
+      'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
+      149.99
+    ),
+    new Place(
+      'p2',
+      'L\'Amour Toujours',
+      'A romattic place in Paris!',
+      'https://live.staticflickr.com/614/31628747276_00dedc1560_b.jpg',
+      189.99
+    ),
+    new Place(
+      'p3',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://i1.trekearth.com/photos/138102/dsc_0681.jpg',
+      99.99
+    ),
+  ];
+
+  get places() {
+    // eslint-disable-next-line no-underscore-dangle
+    return [...this._places];
+  }
 
   constructor() { }
 }
