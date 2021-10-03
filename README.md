9d897af2463aaf0903fbf0201b4294a2ccf12a9c
Yi An Chen <ianchen0419@gmail.com>
2021-10-03 18:24:50 +0800

    add storybook

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ionic-course
+
 Demosite: https://ianchen0419.github.io/ionic-course/
+Storybook: https://main--615982bec67f67003acaaea2.chromatic.com/
---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { IonButton, IonicModule } from '@ionic/angular';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+
+export default {
+  title: 'Ionic/Button',
+  component: IonButton,
+  argTypes: {
+    fill: {
+      options: [ 'solid', 'outline', 'clear' ],
+      control: { type: 'inline-radio' }
+    },
+    color: {
+      options: [ 'primary', 'secondary', 'tertiary', 'success', 'warning', 'danger', 'light', 'medium', 'dark' ],
+      control: { type: 'select' }
+    },
+    mode: {
+      options: [ 'ios', 'md' ],
+      control: { type: 'inline-radio' }
+    },
+    expand: {
+      options: [ 'undefine', 'block', 'full' ],
+      control: { type: 'inline-radio' }
+    },
+    shape: {
+      options: [ 'round' ],
+      control: { type: 'inline-check' }
+    },
+    size: {
+      options: [ 'default', 'large', 'small' ],
+      control: { type: 'inline-radio' }
+    },
+
+  },
+  decorators: [
+    moduleMetadata({
+      imports: [IonicModule.forRoot()],
+    }),
+  ],
+} as Meta;
+
+
+const Template: Story<IonButton> = (args: IonButton) => ({
+  props: args,
+  template: `<ion-button appButton>{{label}}</ion-button>`,
+});
+
+export const Basic = Template.bind({});
+Basic.args = {
+  disabled: false,
+  label: 'Button',
+  fill: 'solid',
+  color: 'primary',
+  mode: 'md',
+  expand: 'undefine',
+  shape: 'undefine',
+  size: 'default',
+};
+
+
+const Template2: Story<IonButton> = (args: IonButton) => ({
+  props: args,
+  template: `<ion-button>你好</ion-button><ion-button>你好</ion-button>`,
+});
+
+export const Two = Template2.bind({});
+Two.args = {
+
+};
